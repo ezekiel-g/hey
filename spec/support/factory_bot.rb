6eb07bd2d995a9c4ffa0f6278b6f58ea626fb9c5
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-10-01 10:51:39 -0400

    Fixed devise-related feature tests

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,8 +3,10 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    username 'username'
     password 'password'
     password_confirmation 'password'
+    role 'member'
   end
 
 end
