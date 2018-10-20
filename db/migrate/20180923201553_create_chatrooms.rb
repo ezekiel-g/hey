5e6b71482cf24dba38c1d880c8649d6fc897bfe7
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-10-20 15:08:01 -0400

    Adjusted db

--- db/migrate/20180923201553_create_chatrooms.rb
+++ db/migrate/20180923201553_create_chatrooms.rb
@@ -2,6 +2,7 @@ class CreateChatrooms < ActiveRecord::Migration[5.2]
   def change
     create_table :chatrooms do |t|
       t.string :name, null: false, unique: true
+      t.integer :owner_id, null: false
 
       t.timestamps null: false
     end
