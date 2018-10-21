f30b87dcdc371bb4496f6a4b3259384608ef9bb2
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-10-20 21:54:04 -0400

    Made it so members can add other members to chatrooms

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,8 @@ class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
+  validates :user_id, uniqueness: { message: "is already a member" }
+
   def username
     User.find_by_id(self.user_id).username
   end
