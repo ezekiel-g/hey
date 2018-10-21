1bcb568c94135b395062444cfd5fff516cf586db
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-10-21 00:45:15 -0400

    Formatted messaged box

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,8 +2,7 @@ class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
-  validates_uniqueness_of :user_id, {scope: :chatroom_id, message: 'is already a member' }
-
+  validates_uniqueness_of :user_id, { scope: :chatroom_id, message: 'is already a member' }
 
   def username
     User.find_by_id(self.user_id).username
